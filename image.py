221717c4179cc06d6bfe5588856f54dfce7134e1
Shubham <shubham07119garg@gmail.com>
2020-12-08 22:07:50 +0530

    add something

--- image.py
+++ image.py
@@ -1,14 +1,44 @@
 import cv2
 import numpy as np
-img = cv2.imread('1.jpg')
+def nothing(x):
+    pass
+img = np.zeros((300,512,3),np.uint8)
+cv2.namedWindow('image')
 
-h,w=img.shape[:2]
-start_row,start_col = int(h*.25),int(w*.25)
-end_row,end_col = int(h*.9),int(w*.5)
-cropped = img[start_row:end_row,start_col:end_col]
+cv2.createTrackbar("R",'image',0,255,nothing)
+cv2.createTrackbar("G",'image',0,255,nothing)
+cv2.createTrackbar("B",'image',0,255,nothing)
+switch = '0 : OFF \n 1 : ON'
+cv2.createTrackbar(switch,'image',0,1,nothing)
+while True:
+    cv2.imshow('image',img)
+    if cv2.waitKey(1)==13:
+        break
+    r =cv2.getTrackbarPos('R','image')
+    g =cv2.getTrackbarPos('G','image')
+    b =cv2.getTrackbarPos('B','image')
+    s =cv2.getTrackbarPos(switch,'image')
 
-cv2.imshow('original',img)
-cv2.waitKey(0)
-cv2.imshow('cropped',cropped)
-cv2.waitKey(0)
+    if s==0:
+        img[:]=0
+    else:
+        img[:] = [b,g,r]
+
+cv2.destroyAllWindows()
+
+    
+device = cv2.VideoCapture(0)
+while True:
+    ret,frame = device.read()
+    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    lr = np.array([110,50,50])
+    ur = np.array([130,250,250])
+    mask = cv2.inRange(hsv,lr,ur)
+    result = cv2.bitwise_and(frame,frame,mask=mask)
+    cv2.imshow("a",mask)
+    cv2.imshow("b",result)
+    cv2.imshow("c",frame)
+    if cv2.waitKey(1)==13:
+        break
+device.release()
 cv2.destroyAllWindows()
